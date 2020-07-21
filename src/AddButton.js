38135df3ab1d27843a6ee10801acade636cde471
Chacko <chackop@gmail.com>
2020-07-21 02:34:12 +0100

    usecallback demo

--- src/AddButton.js
+++ src/AddButton.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 
-export class AddButton extends React.Component {
+export class AddButton extends React.PureComponent {
   render() {
     const { onClick } = this.props;
 
@@ -11,13 +11,13 @@ export class AddButton extends React.Component {
           height: 100,
           width: 100,
           borderRadius: 50,
-          backgroundColor: "#333",
-          position: "absolute",
+          backgroundColor: '#333',
+          position: 'absolute',
           bottom: 100,
           right: 100,
           fontSize: 20,
-          color: "#fff",
-          cursor: "pointer"
+          color: '#fff',
+          cursor: 'pointer',
         }}
       >
         Add
