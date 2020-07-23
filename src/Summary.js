e1dba208d091719ba36942f147a52bb23be2e22f
Chacko <chackop@gmail.com>
2020-07-23 01:09:28 +0100

    added lazy and suspense

--- src/Summary.js
+++ src/Summary.js
@@ -1,37 +1,59 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
 import levenshtein from "levenshtein";
+import "./Summary.css";
+import { CornerButton } from "./CornerButton";
 
 export const Summary = React.memo(
   function Summary(props) {
+    const [position, setPosition] = useState("top-right");
+
     const cards = Object.values(props.cards);
 
-    const distances = { max: 0, min: 100000 };
-    cards.forEach(currentCard => {
-      cards.forEach(compareCard => {
-        if (compareCard === currentCard) {
-          return;
-        }
-        const distance = levenshtein(currentCard.label, compareCard.label);
+    console.time("calc-distances");
+    const distances = useMemo(() => {
+      const distanceCals = { max: 0, min: 100000 };
+      cards.forEach(currentCard => {
+        cards.forEach(compareCard => {
+          if (compareCard === currentCard) {
+            return;
+          }
+          const distance = levenshtein(currentCard.label, compareCard.label);
 
-        distances.max = Math.max(distances.max, distance);
-        distances.min = Math.min(distances.min, distance);
+          distanceCals.max = Math.max(distanceCals.max, distance);
+          distanceCals.min = Math.min(distanceCals.min, distance);
+        });
       });
-    });
+      return distanceCals;
+    }, [Object.keys(props.cards).length]);
+
+    console.timeEnd("calc-distances");
 
     return (
-      <div
-        style={{
-          position: "absolute",
-          right: 20,
-          top: 20,
-          backgroundColor: "#fafafa",
-          padding: "10px",
-          border: "3px solid #333"
-        }}
-      >
+      <div className={`Summary Summary-${position}`}>
         <div>You have {Object.keys(props.cards).length} cards!</div>
         <div>Max difference in labels: {distances.max}</div>
         <div>Min difference in labels: {distances.min}</div>
+
+        <CornerButton
+          setPosition={setPosition}
+          corner="top-right"
+          position={position}
+        />
+        <CornerButton
+          setPosition={setPosition}
+          corner="top-left"
+          position={position}
+        />
+        <CornerButton
+          setPosition={setPosition}
+          corner="bottom-left"
+          position={position}
+        />
+        <CornerButton
+          setPosition={setPosition}
+          corner="bottom-right"
+          position={position}
+        />
       </div>
     );
   },
